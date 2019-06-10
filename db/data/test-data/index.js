5595e244b676fd9d4e3aad1074925e6d1ec3d751
Tom Entwisle <tom@AZEROTH.local>
2019-06-10 17:42:12 +0100

    test data now exported so tests can run on test data... oops

--- db/data/test-data/index.js
+++ db/data/test-data/index.js
@@ -1,1 +1,6 @@
 // require in and export out all test data
+
+exports.articlesData = require("./articles");
+exports.commentsData = require("./comments");
+exports.topicsData = require("./topics");
+exports.usersData = require("./users");
